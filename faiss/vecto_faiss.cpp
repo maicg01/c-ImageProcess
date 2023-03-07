081b7f66062a78337d7ade4a0b331aea6c83caad
maicg01 <nguyenmaicg03@gmail.com>
2023-03-07 17:35:17 +0700

    add fuction load file index read file index va search file index

--- faiss/vecto_faiss.cpp
+++ faiss/vecto_faiss.cpp
@@ -1,9 +1,42 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
 #include <faiss/IndexFlat.h>
+#include <faiss/IndexIVF.h>
+#include <faiss/index_io.h>
 #include <faiss/VectorTransform.h>
 
 using namespace std;
+
+void saveIndex(faiss::IndexFlatL2* index, const char* filename) {
+  // Mở file để ghi dữ liệu
+  FILE* file = fopen(filename, "wb");
+
+  // Lưu trữ đối tượng IndexFlat vào file
+  faiss::write_index(index, file);
+
+  // Đóng file
+  fclose(file);
+}
+
+
+faiss::IndexFlatL2* loadIndex(const char* filename) {
+  // Mở file để đọc dữ liệu
+  FILE* file = fopen(filename, "rb");
+
+  // Tải đối tượng Index vào bộ nhớ
+  faiss::IndexFlatL2* index = dynamic_cast<faiss::IndexFlatL2*>(faiss::read_index(file));
+
+  // Đóng file
+  fclose(file);
+
+  return index;
+}
+
+void searchIndex(faiss::IndexFlat* index, float* query, int nq, int k, float* distances, int64_t* labels) {
+  // Tìm kiếm các vector gần nhất
+  index->search(nq, query, k, distances, labels);
+}
 
 int main() {
   // Tạo ra một vector chứa các giá trị
@@ -41,9 +74,25 @@ int main() {
   cout << "Labels: " << endl;
   cout << *(labels+1) << endl;
 
+  saveIndex(&index,"meme.bin");
+
   // Giải phóng bộ nhớ đã được cấp phát
   delete[] distances;
   delete[] labels;
+
+  faiss::IndexFlatL2* index2;
+  index2 = loadIndex("/home/maicg/Documents/cplus/faiss/build/meme.bin");
+  float* distances2 = new float[1];
+  int64_t* labels2 = new int64_t[1];
+  searchIndex(index2, query.data(),1,2,distances2,labels2);
+
+  cout << "Labels22: " << endl;
+  cout << *(labels2) << endl;
+
+  // Giải phóng bộ nhớ đã được cấp phát
+  delete[] distances2;
+  delete[] labels2;
+
 
   return 0;
 }
